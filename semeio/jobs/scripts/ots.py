8b5d3bda76e2ab00ebd651c720fad9dd922c2278
xjules <parulek@gmail.com>
2020-08-06 13:43:22 +0200

    Renamed ots_job to ots

--- semeio/jobs/scripts/ots.py
+++ semeio/jobs/scripts/ots.py
@@ -5,17 +5,20 @@ import argparse
 from semeio import valid_file
 
 description = (
-        "Overburden timeshift (OTS) generates evolution of reservoir surfaces"
-        "based on eclipse models and seismic velocity volume."
-        "Input yml needs to contains vintages section, where at least one of the four categories"
-        "for the surface computation needs to be set."
-    )
+    "Overburden timeshift (OTS) generates evolution of reservoir surfaces"
+    "based on eclipse models and seismic velocity volume."
+    "Input yml needs to contains vintages section, where at least one of the four categories"
+    "for the surface computation needs to be set."
+)
+
+category = "modeling.surface"
+
 
 def _get_args_parser():
     parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument(
-        "-c", "--config", help="ots config file, yaml format required", type=valid_file,
+        "-c", "--config", help="ots config file, yaml format required", type=valid_file
     )
     return parser
 
